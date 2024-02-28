bdd2d4982743ef66b5eb239b8e98ba47c988fc62
Yuki Ogawa <ogachan6464@gmail.com>
2024-02-28 13:56:08 -0700

    feat: #14 set the order of sidebar

--- app/pages/1_Total.py
+++ app/pages/1_Total.py
@@ -2,11 +2,12 @@ from components.sales import display_sales
 from components.order_date import display_order_date
 from components.payments import display_payments
 
+
 def main():
-  display_sales()
-  display_order_date()
-  display_payments()
+    display_sales()
+    display_order_date()
+    display_payments()
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
