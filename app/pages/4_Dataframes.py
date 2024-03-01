9a9abd11e66f285c256b3f941e0c7ad6fda42ca9
Yuki Ogawa <ogachan6464@gmail.com>
2024-02-29 17:31:54 -0700

    feat: #23 add summary for each dataframe

--- app/pages/4_Dataframes.py
+++ app/pages/4_Dataframes.py
@@ -22,7 +22,24 @@ def display_df_shapes(df_dict):
 def display_dfs(df_dict):
     for df_name, df in df_dict.items():
         st.header(df_name)
-        st.dataframe(df)
+        st.subheader("Summary")
+        df_summary = resumetable(df)
+        st.dataframe(df_summary, height=200)
+        st.subheader("Data")
+        st.dataframe(df, height=200)
+        st.write(f"Shape: {df.shape}")
+
+
+def resumetable(df):
+    summary = pd.DataFrame(df.dtypes, columns=["dtypes"])
+    summary = summary.reset_index()
+    summary["Name"] = summary["index"]
+    summary = summary[["Name", "dtypes"]]
+    summary["Missing"] = df.isnull().sum().values
+    summary["Uniques"] = df.nunique().values
+    summary["First Value"] = df.loc[0].values
+
+    return summary
 
 
 if __name__ == "__main__":
